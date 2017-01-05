197a2cfa8f62b13fcff2ca39bc291159231d2ad1
kritird <kritiranjandas@Kritis-Air.home>
2017-01-05 09:56:32 +0800

    all4

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var fs         = require('fs');
-var eps        = require('ejs');
+var bodyParser   = require('body-parser');
+var fs           = require('fs');
+var eps          = require('ejs');
 var app = express();
 
 Object.assign=require('object-assign')
@@ -57,6 +57,8 @@ var leaderRouter = require('./routes/leaderRouter');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
