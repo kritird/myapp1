8a1dd261bedb04451c3c4607341a25884081d978
kritird <kritiranjandas@Kritis-Air.home>
2017-01-05 09:31:36 +0800

    all32

--- server.js
+++ server.js
@@ -55,11 +55,6 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
